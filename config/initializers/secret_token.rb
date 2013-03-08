48e0f2ba2de5e5644e738251fae55baed0427e77
acoustik <ajay@acoustik.org>
2013-03-08 17:38:14 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Animal::Application.config.secret_token = '3bcad4cdcdbcf269ad6e474636f3ba21d29d8d45eb6f014caae3693b10b3f7dc6d44fca45ee0bd1b2ac2b7efd124bf25c7cd5cc78a926cc92ec2220725f4813d'
