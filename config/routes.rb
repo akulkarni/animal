837ffa34f95eeb811bfc533c7c933dc2687ca807
acoustik <ajay@acoustik.org>
2013-04-13 17:30:28 -0400

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Animal::Application.routes.draw do
   match 'better/redirect' => 'better#redirect'
   match 'better/register' => 'better#register'
+  match 'better/receive_sms' => 'better#receive_sms'
   resources :profile, :better
 end
