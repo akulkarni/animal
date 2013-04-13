8477dc65065aa1f7a61b35d02adc56d4b30b3208
acoustik <ajay@acoustik.org>
2013-04-13 15:28:51 -0400

    now correctly getting foursquare access token and saving.

--- app/controllers/better_controller.rb
+++ app/controllers/better_controller.rb
@@ -6,7 +6,6 @@ class BetterController < ApplicationController
   #  display results on a page
 
   # FIX OAUTH
-  #   doesn't seem to return access_token to redirect_uri
   #   use cookies for user instead of query string param
 
   OAUTH_URI = 'https://foursquare.com/oauth2/authenticate?client_id=%s&response_type=code&redirect_uri=%s/redirect/%s'
@@ -28,7 +27,6 @@ class BetterController < ApplicationController
 
   def oauth(username)
     oauth_uri = OAUTH_URI % [ENV['FOURSQUARE_BETTER_CLIENT_ID'], ENV['BETTER_HOST'], username]
-    puts oauth_uri
     redirect_to oauth_uri
   end
 
@@ -37,14 +35,14 @@ class BetterController < ApplicationController
     code = params['code']
 
     access_token_uri = ACCESS_TOKEN_URI % [ENV['FOURSQUARE_BETTER_CLIENT_ID'], ENV['FOURSQUARE_BETTER_CLIENT_SECRET'], ENV['BETTER_HOST'], username, code]
-    puts access_token_uri
 
     response = Typhoeus.get(access_token_uri)
     access_token = JSON.parse(response.body)['access_token']
+
     unless access_token.nil?
       user = FoursquareUser.new(:username => username.downcase, :access_token => access_token)
       user.save!
-      redirect_to '%s/index?username=%s' % [ENV['BETTER_HOST'], username]
+      redirect_to '%s?username=%s' % [ENV['BETTER_HOST'], username]
     else
       render :text => 'something bad happened.'
     end
