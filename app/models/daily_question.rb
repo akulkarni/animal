87445a03337bd8353a2560f5d614087e8792efb2
acoustik <ajay@acoustik.org>
2013-05-06 08:54:35 -0400

    added 10 more questions

--- app/models/daily_question.rb
+++ app/models/daily_question.rb
@@ -19,7 +19,17 @@ class DailyQuestion < ActiveRecord::Base
                "Spill it.",
                "Today: good, bad, or ugly?",
                "If you were to relive today, what would you do differently?",
-               "What's up?"
+               "What's up?",
+               "What was the most important thing you did today?",
+               "What's your biggest regret of today?",
+               "What conversation today do you remember the most?",
+               "Dude.",
+               "Hit me.",
+               "What's new?",
+               "Who's someone that's close to you, but with whom you haven't spoken in a while? Call them right now.",
+               "Are you happy?",
+               "What are you doing really poorly, that you could do better?",
+               "It's about that time."
               ]
 
 end
